46ebbd38a9cc7ef914242775bdfcb07a09bd16a2
Audrey Renson <audrey.o.renson@gmail.com>
2017-05-08 11:09:27 -0400

    edit_csv.py runs in Python shell to edit csv into form accepted by qryCSV2013 in subDB pre-deployment

--- edit_csv.py
+++ edit_csv.py
@@ -6,16 +6,16 @@ dir = os.path.dirname(__file__)
 filename ='S:/LMC/Clinical Research/Submissions/Submissions Database 2.0 BACKEND/_TEMPDATA/research abstract and publication submission form'
 f = pd.read_csv(filename + ".csv")
 #select only the columns we want
-f = f[[col for col in f.columns if col.startswith("Q") or col in ['V' + n for n in ['1','4','5','8']]]]
+f = f[[col for col in f.columns if col.startswith("Q") or col in ['V' + n for n in ['1','4','5','8']] or col == 'ResponseId']]
 ##rename them
 f = f.rename(columns={'V1':'ResponseID','V4':'Q174','V5':'Q173','V8':'EndDate'})
 #drop the first row
-f = f.drop(f.index[[0]])
+f = f.drop(f.index[[0,1,2]])
 
 #convert phone numbers and pagers to string
 f['Q311'] = f['Q311'].apply(str)
-f['Q312'] = f['Q312'].apply(str)
-f['Q321'] = f['Q321'].apply(str)
+f['Q312'] = f['Q312'].apply(str) 
+f['Q321'] = f['Q321'].apply(str) 
 f['Q322'] = f['Q322'].apply(str)
 
 #delete 'nan's from phone number fields
